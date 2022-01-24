f7f102ba49bc33f8d065ff368f42fb24f631221e
Edoardo Lai <edoardolai96@gmail.com>
2022-01-24 17:51:11 +0100

    push prova

---
+++ main.js
@@ -0,0 +1,14 @@
+const submit= document.getElementById("genera");
+const 
+const nameOutput= document.getElementById("nome-passeggero")
+
+submit.addEventListener("click" , 
+
+function() {
+
+}
+
+)
+
+
+
