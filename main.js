fb9a6277b8c263f2cf50cffaffaecac625979f43
Edoardo Lai <edoardolai96@gmail.com>
2022-01-25 09:43:45 +0100

    Correzione doppi uguali nelle condizioni if

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const genera= document.getElementById("genera");
 
+const annulla = document.getElementById("annulla");
+
 
 const nome = document.getElementById('nome');
 
@@ -9,22 +11,25 @@ const eta = document.getElementById('eta');
 
 const eta_output= document.getElementById("eta_output");
 
-const carrozza_output = document.getElementById("carrozza_output")
+const carrozza_output = document.getElementById("carrozza_output");
 
-const cp_output = document.getElementById("cp_output")
+const cp_output = document.getElementById("cp_output");
 
-const costo_output = document.getElementById("costo_output")
+const costo_output = document.getElementById("costo_output");
 
-const chilometri = document.getElementById("chilometri")
+const chilometri = document.getElementById("chilometri");
+
+
+
 
 
 genera.addEventListener("click", 
 
 function() {
 
-// ripetere per altri valori (input select)
+document.getElementById("prova").classList.remove("d-none");
 
-console.log(nome.value);
+
 nome_output.innerHTML=(nome.value);
 
 eta_output.innerHTML=(eta.value)
@@ -33,20 +38,31 @@ carrozza_output.innerHTML= Math.floor(Math.random() * 10);
 
 cp_output.innerHTML= Math.floor(Math.random(0, 10000) * 10000 + 90000);
 
-let prezzo = chilometri.value * 0.21;
+let prezzo = (chilometri.value * 0.21).toFixed(2);
 
 
 
-if (eta.value = "Minorenne"){
-    costo_output.innerHTML = prezzo * 0.8 ;
+if (eta.value="Minorenne"){
+    costo_output.innerHTML = (prezzo * 0.8).toFixed(2) ;
+    eta.value = "Biglietto Standard";
 
 } else if (eta.value ="65+") {
-    costo_output.innerHTML = Math.floor(prezzo * 0.6) ;
+    costo_output.innerHTML = (prezzo * 0.6).toFixed(2) ;
+    eta.value = "Biglietto Standard";
 
+}else{ costo_output.innerHTML= prezzo;
 }
 
 }
 
+) 
+
+annulla.addEventListener("click", 
+function() {
+nome.value=" ";
+eta.value="Biglietto Standard";
+chilometri.value=""
+}
 )
 
 
